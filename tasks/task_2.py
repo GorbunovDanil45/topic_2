360bf4824db5c8bcc59e43c60d0f0454f2fc59ce
Gorbunov Danil <danil45@bk.ru>
2025-04-27 20:36:43 +0500

    исправление задач 2, 9

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,7 +1,5 @@
 print("Кнопочный телефон:\n")
-print("1\t2\t3")
-print("4\t5\t6")
-print("7\t8\t9")
-print("*\t0\t#")
-
 print(1, 2, 3, sep='\t')
+print(4, 5, 6, sep='\t')
+print(7, 8, 9, sep='\t')
+print('*', 0, '#', sep='\t')
