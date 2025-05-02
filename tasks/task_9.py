091db4e56368e40c50d705d4fab65de98e8b8fc2
Gorbunov Danil <danil45@bk.ru>
2025-05-02 19:59:30 +0500

    исправление задачи 9

--- tasks/task_9.py
+++ tasks/task_9.py
@@ -4,7 +4,7 @@ kilo_size = byte_size / 1024
 mega_size = kilo_size / 1024
 giga_size = mega_size / 1024
 
-print('Объём информации ' + str(byte_size) + ' байт' + '\n'
+print('Объём информации ' + str(byte_size) + ' байт\n'
       + 'В килобайтах: ' + str(kilo_size) + '\n'
       + 'В мегабайтах: ' + str(mega_size) + '\n'
       + 'В гигабайтах: ' + str(giga_size) + '\n')
